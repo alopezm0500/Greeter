66b58a5662ae81b6cd719f79db51ec1f80618f79
alopezm0500 <62066710+alopezm0500@users.noreply.github.com>
2022-06-09 13:22:05 -0500

    Update Greeter.sol
    this is the hello world in a smart contract matter

--- Greeter.sol
+++ Greeter.sol
@@ -1,15 +1,14 @@
-pragma solidity >=0.4.0;
+pragma solidity ^0.4.0;
 contract Greeter {
     string public yourName;
-    function Greeter() public{
+    function Greeter1() public{
     yourName="World";
     }
     function set(string name) public{
     yourName=name;
     }
     
-    function hello() constant returns (string){
+    function hello() constant public returns (string){
     return yourName;
     }
-
 }
